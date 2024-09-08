359082c49a21d12a7b4801f64be845e346f9e36c
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-09-09 00:24:14 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+<div align="center">
+
+# asdf-any-cargo-quickinstall ![Test](https://github.com/vic1707/asdf-any-cargo-quickinstall/workflows/Test/badge.svg) ![Lint](https://github.com/vic1707/asdf-any-cargo-quickinstall/workflows/Lint/badge.svg)
+
+A flexible [asdf](https://asdf-vm.com) plugin for installing any tool available via [cargo-quickinstall](https://github.com/cargo-bins/cargo-quickinstall).
+
+</div>
+
+# Contents
+
+-   [Dependencies](#dependencies)
+-   [Install](#install)
+-   [Usage](#usage)
+-   [Contributing](#contributing)
+-   [License](#license)
+
+# Dependencies
+
+-   `bash`, `curl` or `wget`, `tar`.
+
+# Install
+
+You can install this plugin to manage any Rust-based tool available in the [cargo-quickinstall](https://github.com/cargo-bins/cargo-quickinstall) repository.
+The plugin is dynamic, meaning it can be used for multiple tools without needing to hard-code each one.
+
+In the following 2 sections, you'll need to replace `<cargo-quickinstall-tool>` with the actual tool name you wish to install.
+
+```shell
+asdf plugin add <cargo-quickinstall-tool> https://github.com/vic1707/asdf-any-cargo-quickinstall.git
+```
+
+# Usage
+
+```shell
+# Show all installable versions
+asdf list-all <cargo-quickinstall-tool>
+
+# Install specific version
+asdf install <cargo-quickinstall-tool> latest
+
+# Set a version globally (on your ~/.tool-versions file)
+asdf global <cargo-quickinstall-tool> latest
+
+# Now your tool's commands are available
+<cargo-quickinstall-tool> --version
+```
+
+Check [asdf](https://github.com/asdf-vm/asdf) readme for more instructions on how to
+install & manage versions.
+
+# Known limitations
+
+This plugin depends heavily on what [cargo-quickinstall](https://github.com/cargo-bins/cargo-quickinstall) pre-builds.
+
+For example, [bacon](https://github.com/Canop/bacon) in its [quickinstall 2.20.0](https://github.com/cargo-bins/cargo-quickinstall/releases/bacon-2.20.0) does not provide a linux `x86_64` binary using `glibc`, only `musl`.
+Consequently, this plugin is unable to install [bacon](https://github.com/Canop/bacon) on `glibc`-dependent distros.
+
+# Possible improvements
+
+-   [ ] -   Try to make the scripts POSIX `sh` compliant because.
+-   [ ] -   Check all scripts against [asdf banned commands](https://github.com/asdf-vm/asdf/blob/master/test/banned_commands.bats).
+
+# Contributing
+
+Contributions of any kind welcome!
+
+[Thanks goes to these contributors](https://github.com/vic1707/asdf-any-cargo-quickinstall/graphs/contributors)!
+
+# License
+
+`asdf-any-cargo-quickinstall` is highly inspired by [asdf-eza](https://github.com/lwiechec/asdf-eza) by [lwiechec](https://github.com/lwiechec) which is itself almost a direct copy of [asdf-zoxide](https://github.com/nyrst/asdf-zoxide) by [nyrst](https://github.com/nyrst); kudos!
+
+See [LICENSE](LICENSE) © [Siegfried Ehret](https://github.com/SiegfriedEhret/)
---
+++ lib/utils.bash
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# TOOL_NAME="$(basename "$(dirname "$(dirname "$0")")")"
+TOOL_NAME="${0%/*/*}"
+TOOL_NAME="${TOOL_NAME##*/}"
+
+say() {
+	echo "[asdf-any-cargo-quickinstall]($TOOL_NAME): $*"
+}
+
+fail() {
+	say "$*" >&2
+	exit 1
+}
+
+sort_semver() {
+	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' \
+		| LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n \
+		| awk '{print $2}'
+}
+
+list_github_semver_tags() {
+	gh_repo="$1"
+	tool_name="$2"
+
+	git ls-remote --tags --refs "$gh_repo" \
+		| grep -o "refs/tags/$tool_name-.*" \
+		| cut -d/ -f3- \
+		| sed "s/^$tool_name-//" \
+		| grep -oE '^[0-9]+\.[0-9]+\.[0-9]+$' \
+		| uniq
+}
+
+is_valid() {
+	url="$1"
+	if which curl > /dev/null; then
+		curl -s --fail "$url"
+	elif which wget > /dev/null; then
+		wget -q --spider "$url" &> /dev/null
+	else
+		fail "Error: Neither curl nor wget is available."
+	fi
+}
+
+download() {
+	out_file="$1"
+	url="$2"
+
+	if which curl > /dev/null; then
+		curl -fsSL ${GITHUB_API_TOKEN:+-H "Authorization: token $GITHUB_API_TOKEN"} -o "$out_file" "$url"
+	elif which wget > /dev/null; then
+		wget -q ${GITHUB_API_TOKEN:+--header="Authorization: token $GITHUB_API_TOKEN"} -O "$out_file" "$url"
+	else
+		fail "Error: Neither curl nor wget is available."
+	fi
+}
+
+# returns an array of compatible triples (or quadruples depending on linux's libc)
+get_target_triples() {
+	platform=$(uname -s)
+	arch=$(uname -m)
+
+	case "$platform" in
+		Darwin)
+			case "$arch" in
+				aarch64 | arm64) targets=("aarch64-apple-darwin" "x86_64-apple-darwin") ;; # Rosetta
+				x86_64) targets=("x86_64-apple-darwin") ;;
+				*) fail "Incompatible darwin architecture." ;;
+			esac
+			;;
+		Linux)
+			case "$arch" in
+				aarch64 | arm64) base_triple="aarch64-unknown-linux" ;;
+				armv7l | armv7) base_triple="armv7-unknown-linux" ;;
+				x86_64) base_triple="x86_64-unknown-linux" ;;
+				*) fail "Incompatible linux architecture." ;;
+			esac
+
+			libc_sufix=""
+			[ "$arch" = "armv7" ] || [ "$arch" = "armv7l" ] && libc_sufix="eabihf"
+
+			# Detect libc libraries for musl and glibc
+			musl_lib="/lib/ld-musl-$arch.so.1"
+			glibc_lib="/lib/ld-linux-$arch.so.1"
+
+			# Adjust glibc path for specific architectures
+			case "$arch" in
+				x86_64) glibc_lib="/lib64/ld-linux-x86-64.so.2" ;;
+				aarch64 | arm64) glibc_lib="/lib/ld-linux-aarch64.so.1" ;;
+				armv7l | armv7) glibc_lib="/lib/ld-linux-armhf.so.3" ;;
+			esac
+
+			musl_exists=$([ -f "$musl_lib" ] && echo 1 || echo 0)
+			glibc_exists=$([ -f "$glibc_lib" ] && echo 1 || echo 0)
+
+			case "$musl_exists:$glibc_exists" in
+				1:1) targets=("$base_triple-musl$libc_sufix" "$base_triple-gnu$libc_sufix") ;;
+				1:0) targets=("$base_triple-musl$libc_sufix") ;;
+				0:1) targets=("$base_triple-gnu$libc_sufix") ;;
+				0:0) fail "Neither musl nor glibc detected." ;;
+			esac
+			;;
+		*) fail "Incompatible platform." ;;
+	esac
+
+	echo "${targets[@]}"
+}
