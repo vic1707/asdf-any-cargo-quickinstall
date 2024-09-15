2195c4447f4dcd0568cd5142cfdb1da61cea054a
Victor LEFEBVRE <dev@vic1707.xyz>
2024-09-15 21:16:04 +0200

    fix semver sorting (#3)

--- lib/utils.bash
+++ lib/utils.bash
@@ -16,9 +16,7 @@ fail() {
 }
 
 sort_semver() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' \
-		| LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n \
-		| awk '{print $2}'
+	sort -t. -k 1,1n -k 2,2n -k 3,3n
 }
 
 list_github_semver_tags() {
